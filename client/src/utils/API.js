68d883f5e28d58c0d74942c161541efaabb2cc4d
Lisa Vinson <Lrv0828@gmail.com>
2018-03-23 21:27:46 -0400

    added Article model, updated post route to be able to add Article based on Save button click

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,12 +7,22 @@ export default {
         return (axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931&q=" + [topic] + "&begin_date=" + [startYear] + "0101" + "&end_date=" + [endYear] + "0101" + "&fq=document_type:(article)"));
     },
 
-    saveArticle: (articleData) => {
+    saveArticle: (data) => {
         // console.log("title inside API Save Article", article.headline);
         // console.log("btnId");
 
 
-        console.log("Inside saveArticle function");
+
+        // console.log("Inside saveArticle function:", data);
+        const articleData = {
+            headline: data.headline.main,
+            byline: data.byline.original,
+            pub_date: data.pub_date,
+            snippet: data.snippet,
+            url: data.web_url
+        }
+console.log("publishing date:", articleData.pub_date);
+console.log(typeof articleData.pub_date);
         return (axios.post("/api/article", articleData));
         //Add functionality - post request using axios to the routes, routes to controller file. Create a new instance of Article schema, save to mongo DB, return the saved object, pass back in a promise in Home, update state (last Saved),
         //Update panel 3 to do a get request for articles saved in the db and display
--- controllers/articleController.js
+++ controllers/articleController.js
@@ -10,9 +10,9 @@ module.exports = {
     },
 
     create: (req, res) => {
-        console.log("Inside create method of article Controller");
-        db.Article.create(req.body)
-        .then(response => req.json(response))
+        console.log("Inside create method of article Controller", req.body);
+        db.Article.create({headline: req.body.headline, publishDate: req.body.pub_date, url: req.body.url, author: req.body.byline})
+        .then(response => console.log(response))
         .catch(err => res.status(422).json(err))
     }
 
