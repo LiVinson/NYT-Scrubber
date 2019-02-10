646d76591b3fe6e9c39f53712924fd41595d3594
Lisa Vinson <Lrv0828@gmail.com>
2019-02-09 19:35:21 -0500

    updated api key, fixed button css focus issues, added body padding

--- client/src/components/SearchForm/SearchForm.js
+++ client/src/components/SearchForm/SearchForm.js
@@ -2,46 +2,47 @@ import React from "react";
 import "./SearchForm.css";
 
 const SearchForm = props => {
- return (
-  <form className="search">
-    <div className="form-group">
-      <label>Topic</label>
-      <input
-        name="topic"
-        type="text"
-        onChange={props.handleInputChange}
-        value={props.search}
-        className="form-control"
-        placeholder="Popular Searches: Florida, election, Facebook..."
-      />
-      <label>Start Year</label>
-      <input
-        name="startYear"
-        type="text"
-        onChange={props.handleInputChange}
-        value={props.startYear}
-        className="form-control"
-        placeholder="2017"
-        
-      />
-      <label>End Year</label>
-      <input
-        name="endYear"
-        type="text"
-        onChange={props.handleInputChange}
-        value={props.startYear}
-        className="form-control"
-        placeholder="2018"
-      />
-    </div>
+  return (
+    <form className="search">
+      <div className="form-group">
+        <label>Topic</label>
+        <input
+          name="topic"
+          type="text"
+          onChange={props.handleInputChange}
+          value={props.search}
+          className="form-control"
+          placeholder="Popular Searches: Florida, election, Facebook..."
+        />
+        <label>Start Year</label>
+        <input
+          name="startYear"
+          type="text"
+          onChange={props.handleInputChange}
+          value={props.startYear}
+          className="form-control"
+          placeholder="2017"
+        />
+        <label>End Year</label>
+        <input
+          name="endYear"
+          type="text"
+          onChange={props.handleInputChange}
+          value={props.startYear}
+          className="form-control"
+          placeholder="2018"
+        />
+      </div>
 
-    <button type="submit" onClick={props.handleFormSubmit} className="btn submitBtn">
-      Find Articles
-    </button>
-
-
-  </form>
-)
+      <button
+        type="submit"
+        onClick={props.handleFormSubmit}
+        className="submitBtn btn"
+      >
+        Find Articles
+      </button>
+    </form>
+  );
 };
 
 export default SearchForm;
